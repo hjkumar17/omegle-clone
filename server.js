99cdff4231bf9bca7f0fc2efa78409e49f282b85
Hemant <hjkumar17@gmail.com>
2023-07-11 00:07:24 +0530

    create and answer offer part done

--- server.js
+++ server.js
@@ -14,6 +14,59 @@ app.use("/css", express.static(path.resolve(__dirname, "Assets/css")));
 
 app.use("/", require("./Server/routes/router"));
 
-app.listen(PORT,() => {
+const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+  allowEI03: true,
+});
+
+let userConnections = [];
+
+io.on("connection", (socket) => {
+  // console.log("socket id is", socket);
+  socket.on("userConnected", (data) => {
+    userConnections.push({
+      connectionId: socket.id,
+      userId: data.displayName,
+    });
+  });
+
+  socket.on("offerSentToRemote", (data) => {
+    let offerReceiver = userConnections.find(
+      (users) => users.userId === data.remoteUser
+    );
+
+    if (offerReceiver) {
+      socket.to(offerReceiver.connectionId).emit("receiveOffer", data);
+    }
+  });
+
+  socket.on("sendAnswerToUser1", (data) => {
+    let answerReciver = userConnections.find(
+      (users) => users.userId === data.receiver
+    );
+
+    if (answerReciver) {
+      socket.to(answerReciver.connectionId).emit("receiverAnswer", data);
+    }
+  });
+
+  socket.on("candidateSentToUser", (data) => {
+    let candidateReciver = userConnections.find(
+      (users) => users.userId === data.remoteUser
+    );
+
+    if (candidateReciver) {
+      socket.to(candidateReciver.connectionId).emit("candidateReciver", data);
+    }
+  });
+
+  socket.on('disconnect',()=>{
+    let disUser = userConnections.find(user=>user.connectionId === socket.id)
+    if(disUser){
+      userConnections = userConnections.filter(user=>user.connectionId !== socket.id)
+    }
+  })
 });
